ab6cac6777f465a8c7c9b43d5001ad189f41c587
김성재 <tjdwo6269@naver.com>
2024-06-17 12:53:26 +0900

    Add : apiGateway User 로드밸런싱, 필터 임시 주석 처리

--- Auth/src/main/java/shop/msa/auth/filter/JwtFilter.java
+++ Auth/src/main/java/shop/msa/auth/filter/JwtFilter.java
@@ -1,46 +1,46 @@
-package shop.msa.auth.filter;
-
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import shop.msa.auth.util.JwtUtil;
-
-@Component
-public class JwtFilter extends AbstractGatewayFilterFactory<JwtFilter.Config> {
-
-    private JwtUtil jwtUtil;
-
-    public JwtFilter(JwtUtil jwtUtil) {
-        super(Config.class);
-        this.jwtUtil = jwtUtil;
-    }
-
-    public static class Config {
-
-    }
-
-    @Override
-    public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
-
-            try {
-                String token = exchange.getRequest().getHeaders().get("Authorization").get(0);
-                String userName = jwtUtil.getUsername(token);
-                // 별도의 인증 로직 추가 필요
-                exchange.getRequest()
-                        .mutate()
-                        .header("username",userName)
-                        .build();
-            } catch(Exception e) {
-                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                return exchange.getResponse().setComplete();
-            }
-
-            return chain.filter(exchange);
-        };
-    }
-
-
-
-}
+//package shop.msa.auth.filter;
+//
+//import org.springframework.cloud.gateway.filter.GatewayFilter;
+//import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Component;
+//import shop.msa.auth.util.JwtUtil;
+//
+//@Component
+//public class JwtFilter extends AbstractGatewayFilterFactory<JwtFilter.Config> {
+//
+//    private JwtUtil jwtUtil;
+//
+//    public JwtFilter(JwtUtil jwtUtil) {
+//        super(Config.class);
+//        this.jwtUtil = jwtUtil;
+//    }
+//
+//    public static class Config {
+//
+//    }
+//
+//    @Override
+//    public GatewayFilter apply(Config config) {
+//        return (exchange, chain) -> {
+//
+//            try {
+//                String token = exchange.getRequest().getHeaders().get("Authorization").get(0);
+//                String userName = jwtUtil.getUsername(token);
+//                // 별도의 인증 로직 추가 필요
+//                exchange.getRequest()
+//                        .mutate()
+//                        .header("username",userName)
+//                        .build();
+//            } catch(Exception e) {
+//                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+//                return exchange.getResponse().setComplete();
+//            }
+//
+//            return chain.filter(exchange);
+//        };
+//    }
+//
+//
+//
+//}
