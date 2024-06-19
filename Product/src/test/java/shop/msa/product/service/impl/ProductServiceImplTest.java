208fe8750d8a584f0a10660feb50d6cee393bee8
김성재 <tjdwo6269@naver.com>
2024-06-19 18:59:21 +0900

    Add : Product 등록 관련 추가 테스트 코드 작성

--- Product/src/test/java/shop/msa/product/service/impl/ProductServiceImplTest.java
+++ Product/src/test/java/shop/msa/product/service/impl/ProductServiceImplTest.java
@@ -8,12 +8,15 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 import shop.msa.product.controller.request.ProductCreateRequest;
 import shop.msa.product.domain.Product;
+import shop.msa.product.exception.CustomException;
 import shop.msa.product.service.CategoryService;
 import shop.msa.product.service.ProductService;
+import shop.msa.product.service.cqrs.CategoryQueryPort;
 import shop.msa.product.service.cqrs.ProductQueryPort;
 import shop.msa.product.service.request.CategoryServiceCreateRequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -25,6 +28,8 @@ class ProductServiceImplTest {
     ProductService productService;
     @Autowired
     ProductQueryPort productQueryPort;
+    @Autowired
+    CategoryQueryPort categoryQueryPort;
 
     @Test
     @DisplayName("상품 등록 성공")
@@ -40,5 +45,34 @@ class ProductServiceImplTest {
 
         //then
         assertThat(product.getName()).isEqualTo("등록");
+    }
+
+    @Test
+    @DisplayName("최하위 카테고리가 아닌 상품 등록 실패")
+    public void createProductNotLowestCategory() {
+        //given
+        CategoryServiceCreateRequest request1 = new CategoryServiceCreateRequest(null, "cloth");
+        categoryService.create(request1);
+        CategoryServiceCreateRequest request2 = new CategoryServiceCreateRequest(categoryQueryPort.findByName("cloth").get().getId(), "bottom");
+        CategoryServiceCreateRequest request3 = new CategoryServiceCreateRequest(categoryQueryPort.findByName("cloth").get().getId(), "top");
+        categoryService.create(request2);
+        categoryService.create(request3);
+
+        ProductCreateRequest request = new ProductCreateRequest("등록","cloth",3000,30);
+
+        //when & then
+        assertThrows(CustomException.class, () -> productService.create(request.toServiceRequest()));
+    }
+
+    @Test
+    @DisplayName("동일한 상품 이름 등록 실패")
+    public void createDuplicateProduct() {
+        //given
+        CategoryServiceCreateRequest c = new CategoryServiceCreateRequest(null, "cloth");
+        categoryService.create(c);
+        ProductCreateRequest request1 = new ProductCreateRequest("등록","cloth",3000,30);
+        //when & then
+        productService.create(request1.toServiceRequest());
+        assertThrows(CustomException.class, () -> productService.create(request1.toServiceRequest()));
     }
 }
