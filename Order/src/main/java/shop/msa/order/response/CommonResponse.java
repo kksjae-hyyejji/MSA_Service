d168b74d32e75e84c3173a25e19e3c2e6b37f55c
김성재 <tjdwo6269@naver.com>
2024-06-26 15:31:57 +0900

    Add : Order 서버 기본 틀 작성

---
+++ Order/src/main/java/shop/msa/order/response/CommonResponse.java
@@ -0,0 +1,24 @@
+package shop.msa.order.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+
+import java.util.Map;
+
+@Getter
+@NoArgsConstructor
+public class CommonResponse {
+
+    private String message;
+
+    @Singular("data")
+    private Map<String, Object> data;
+
+    @Builder
+    public CommonResponse(String message, Map<String, Object> data) {
+        this.message = message;
+        this.data = data;
+    }
+}
